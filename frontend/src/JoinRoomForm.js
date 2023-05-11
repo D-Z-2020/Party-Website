b389a47266ff157f1e93105dd9c56f87db3cd32d
twan108 <twan108@aucklanduni.ac.nz>
2023-05-12 03:00:44 +0800

    update join room styling

--- frontend/src/JoinRoomForm.js
+++ frontend/src/JoinRoomForm.js
@@ -2,35 +2,36 @@ import React from 'react'
 
 export default function JoinRoomForm({ joinRoom, roomIdToJoin, setRoomCodeToJoin, setShowJoinRoomForm }) {
     return (
-        <div className="container p-2">
-			<div class="card">
-			<div class="card-body">
+        <div className="container p-2 d-flex align-items-center justify-content-center" style={{ height: "80vh"}}>
+			{/* <div class="card">
+			<div class="card-body"> */}
             <div className="row justify-content-center">
                 <div className="col-md-10 text-center mt-5">
-                    <h1>Enter 4 Digits Room Code</h1>
+                    <h1>Join a party</h1>
                 </div>
-                <div className="col-md-6">
+                <div className="col-md-6 text-center">
                     <form onSubmit={(e) => joinRoom(e)}>
                         <div className="form-group my-3">
                             <input
-                                className="form-control"
+                                className="short-input"
                                 value={roomIdToJoin}
                                 onChange={(e) => setRoomCodeToJoin(e.target.value)}
                                 type="text"
-                                placeholder='room id to join'
+                                placeholder='4 Digits Party Code'
                                 required={true} />
                         </div>
                         <div className="form-group">
-                            <input className='btn btn-primary w-100 my-3' type="submit" value="JOIN" />
+                            <input className='button btn btn-primary my-3' type="submit" value="Join" />
+                        </div>
+                        <div className="form-group">
+                            <input className="btn-secondary my-3" type="button" value="Back" onClick={() => setShowJoinRoomForm(false)} />
                         </div>
                     </form>
-                    <div className="form-group">
-                        <input className="btn btn-primary w-100 my-3" type="button" value="back" onClick={() => setShowJoinRoomForm(false)} />
-                    </div>
+                    
                 </div>
             </div>
-			</div>
-            </div>
+			{/* </div>
+            </div> */}
         </div>
 
     )
