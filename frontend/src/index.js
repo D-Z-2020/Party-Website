aaeba293b9ec738bf124648009e92982aa8f777a
twan108 <twan108@aucklanduni.ac.nz>
2023-05-12 02:54:21 +0800

    update font

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "@fontsource/dm-sans";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
