b53f52f3d5f675356189a43a515f6bddb209d789
twan108 <twan108@aucklanduni.ac.nz>
2023-05-12 17:22:26 +0800

    update image styling

--- frontend/src/RoomImages.js
+++ frontend/src/RoomImages.js
@@ -44,24 +44,22 @@ const RoomImages = ({ roomId, handleImageDeleted, isHost }) => {
     };
 
     return (
-        <div>
-
-            <div>
-                {images.map((image, index) => (
-                    <div key={index} style={{ display: 'inline-block', margin: '10px' }}>
-                        <img
-                            src={`http://localhost:3001${image}`}
-                            alt={`Room-${roomId}-Image${index + 1}`}
-                            style={{ width: '250px' }}
-                        />
-                        <br />
-                        <div className='text-center'>
+        <div className='text-center'>
+            {images.map((image, index) => (
+                <div key={index} style={{ display: 'inline-block', margin: '10px' }}>
+                    <img
+                        src={`http://localhost:3001${image}`}
+                        alt={`Room-${roomId}-Image${index + 1}`}
+                        style={{ width: '250px' }}
+                    />
+                    <br />
+                    <div className='text-center'>
                         <a href={`http://localhost:3001${image}`} download={`Room-${roomId}-Image-${index + 1}`} target="_blank" rel="noopener noreferrer">
-                            <button className='btn btn-secondary ml-5' style={{ display: 'inline', border: 'none', cursor: 'pointer'}} >
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"/>
-  <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"/>
-</svg>
+                            <button className='btn btn-secondary ml-5' style={{ display: 'inline', border: 'none', cursor: 'pointer' }} >
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z" />
+                                    <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z" />
+                                </svg>
                             </button>
                         </a>
                         <button className='btn btn-add ml-5' style={{ display: 'inline', border: 'none', cursor: 'pointer', marginLeft: '0.4rem' }} onClick={() => downloadImage(`http://localhost:3001` + image, `Room-${roomId}-Image-${index + 1}`)}>
@@ -78,10 +76,9 @@ const RoomImages = ({ roomId, handleImageDeleted, isHost }) => {
                                 <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
                             </svg>
                         </button>}
-                        </div>
                     </div>
-                ))}
-            </div>
+                </div>
+            ))}
         </div>
     );
 };
