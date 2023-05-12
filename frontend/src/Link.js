61d88732e5cda49a8d6888278b49dfeee3a69229
D-Z-2020 <zzha248@aucklanduni.ac.nz>
2023-05-12 19:50:25 +0800

    fix bugs

--- frontend/src/Link.js
+++ frontend/src/Link.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Link({ link, deleteLink, isHost }) {
   return (
     <div className='row' style={{width:"100%", fontSize:'1.2rem', marginTop: '1rem'}}>
-      <a className='col-7' style={{ display: 'inline' }} href={"https://" + link}>{link}</a>
+      <a className='col-7' style={{ display: 'inline' }} target='_blank' href={"https://" + link}>{link}</a>
       <div className='col-3'></div>
       {isHost && <button
         className='col-2'
